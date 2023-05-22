7b1c5458bca6bea04fb575d017b9d6030cca454b
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-22 20:52:16 +0300

    remove unused xml

--- src/main/java/com/example/simulinkviewer/HelloApplication.java
+++ src/main/java/com/example/simulinkviewer/HelloApplication.java
@@ -3,6 +3,8 @@ package com.example.simulinkviewer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +12,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        StackPane pane = new StackPane();
+
+        pane.getChildren().add(new Button("OK"));
+
+        Button btnOK = new Button("OK");
+        Scene scene = new Scene(pane, 500, 500);
+        stage.setTitle("SimulinkViewer");
         stage.setScene(scene);
         stage.show();
     }
