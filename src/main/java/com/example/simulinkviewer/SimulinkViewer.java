4132e3bf9e36806272a95e00da864ce9434528f6
MahmoudHamdy02 <mahmoud-mahgoub1@hotmail.com>
2023-05-22 20:57:33 +0300

    rename file, create FileReader

--- src/main/java/com/example/simulinkviewer/SimulinkViewer.java
+++ src/main/java/com/example/simulinkviewer/SimulinkViewer.java
@@ -1,7 +1,6 @@
 package com.example.simulinkviewer;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
@@ -9,7 +8,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class SimulinkViewer extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         StackPane pane = new StackPane();
